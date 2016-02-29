e4ae2e0b8cc96386b56fced82f5b4a2ccaf9345b
iwuvhugs <ikirill2323@gmail.com>
2016-02-29 14:57:40 -0500

    Initial Commit

---
+++ MidtermPractice2016/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MidtermPractice2016
+//
+//  Created by Kirill Suslov on 2016-02-29.
+//  Copyright © 2016 Kirill Suslov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
