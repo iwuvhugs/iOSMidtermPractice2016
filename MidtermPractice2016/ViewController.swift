b557abc6d4fe800d7e336a6f5bc9fbc2cb559f0a
iwuvhugs <ikirill2323@gmail.com>
2016-03-31 23:44:54 -0400

    Initial commit

--- MidtermPractice2016/ViewController.swift
+++ MidtermPractice2016/ViewController.swift
@@ -8,18 +8,67 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    let formatter = NSNumberFormatter()
+    
+    var rotValue:Int{
+        get{
+            return formatter.numberFromString(rotTextField.text!)!.integerValue
+        }
+        set{
+            rotTextField.text = formatter.stringFromNumber(newValue)
+        }
+    }
+    
+    @IBOutlet weak var originalText: UITextField!
+    @IBOutlet weak var cipherText: UITextField!
+    @IBOutlet weak var rotTextField: UILabel!
+    
+    @IBAction func cipher(sender: UIButton) {
+        rotateText()
+    }
+    
+    @IBAction func rot(sender: UISlider) {
+        rotValue = Int(sender.value)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        originalText.delegate = self
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true;
     }
-
-
+    
+    func dismissKeyboard(){
+        view.endEditing(true)
+    }
+    
+    let letters = NSCharacterSet.letterCharacterSet()
+    func rotateText(){
+        
+        let original = originalText.text!
+        var result = [Character]()
+        
+        for char in original.unicodeScalars {
+            var val = UInt32(char)
+            if letters.longCharacterIsMember(val){
+                val +=  UInt32(rotValue)
+                if !letters.longCharacterIsMember(val){
+                    val -=  UInt32(26)
+                }
+            }
+            result.append(Character(UnicodeScalar(val)))
+        }
+        
+        cipherText.text = String(result)
+    }
 }
 
